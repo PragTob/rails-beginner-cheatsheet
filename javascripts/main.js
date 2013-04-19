d489a32ea84ed818d6f070e017521e18224c0c75
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-04-19 19:09:26 +0200

    Integrated bootstrap and tehrebye threw away most of the template
    * New read design with a little top navbar
    * newer cooler tables
    * jshack to scroll to the right position on anchor jumps
    * included jquery and bootstrap and stuff
    * lots of original style sheet things deleted
    * I believe bootstrap.js isn't even used atm ;-)

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+// no overlap when jumping to anchors thanks to:
+// https://github.com/twitter/bootstrap/issues/1768#issuecomment-6531466
+var shiftWindow = function() { scrollBy(0, -40) };
+window.addEventListener("hashchange", shiftWindow);
+$(function(){
+  if (window.location.hash) {
+    shiftWindow();
+  }
+});
